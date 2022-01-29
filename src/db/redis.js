eb10eb5eed94679cac01e9ba4f9e9f59ff04e31a
nguyenvantiennngithub <nguyenvantiennn0910@gmail.com>
2022-01-29 11:19:14 +0700

    debugs

--- src/db/redis.js
+++ src/db/redis.js
@@ -1,6 +1,21 @@
 import { createClient } from 'redis';
 
-const redisClient = createClient(process.env.REDIS_TLS_URL);
+var redisClient;
+
+if (process.env.REDIS_TLS_URL){
+    console.log("RUN REDIS ON PRODUCTION")
+    redisClient = createClient({
+        url: process.env.REDIS_TLS_URL,
+        socket: {
+            tls: true,
+            rejectUnauthorized: false
+        }
+    })
+}else{
+    console.log("RUN REDIS ON LOCAL")
+    redisClient = createClient(process.env.REDIS_TLS_URL);
+}
+
 redisClient.on('error', (err) => console.log('Redis Client Error', err));
 redisClient.connect().then(()=>{
     console.log("Redis connect success");
